b1127777eacab9a0638fead0036c2f52e47073d5
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-02-27 15:07:04 -0500

    Refactored App to be a class function. App is now getting friends data from the local server and using it to setState.

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: []
+    };
+  }
+  componentDidMount() {
+    console.log("CDM now running");
+    // http://localhost:3333 is the address to the server doorstep
+    // /items is the "endpoint"
+    axios
+      .get("http://localhost:5000/friends")
+      .then(res => {
+        console.log(res);
+        this.setState({ friends: res.data });
+      })
+      .catch(err => {
+        console.log(err);
+        // this.setState({ error: err });
+      });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <header className="App-header" />
       </div>
     );
   }
